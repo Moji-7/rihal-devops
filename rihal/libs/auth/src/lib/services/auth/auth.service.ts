e2d41e87e2fe5af2acce4abeeeeb95c9f04e9233
Moji <mirzaei_7@yahoo.com>
2022-08-29 10:52:14 +0430

    Revert "add service and components to Auth lib"
    This reverts commit 50b7ef1ad3a7d3255ee0a3779e2550aa1977c4d0.

---
+++ rihal/libs/auth/src/lib/services/auth/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Authenticate, User } from '@rihal/data-models';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor(private httpClient: HttpClient) {}
+
+  login(authenticate: Authenticate): Observable<User> {
+    return this.httpClient.post<User>(
+      'http://localhost:3000/login',
+      authenticate
+    );
+  }
+
+}
