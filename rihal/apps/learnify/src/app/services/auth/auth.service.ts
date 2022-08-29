50b7ef1ad3a7d3255ee0a3779e2550aa1977c4d0
Moji <mirzaei_7@yahoo.com>
2022-08-29 10:27:10 +0430

    add service and components to Auth lib
    2 components
    1 service

---
+++ rihal/apps/learnify/src/app/services/auth/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Authenticate } from '@demo-app/data-models';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor(private httpClient: HttpClient) {}
+
+  login(authenticate: Authenticate): Observable<any> {
+    return this.httpClient.post('http://localhost:3000/login', authenticate);
+  }
+}
